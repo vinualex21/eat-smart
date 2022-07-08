d96cdd34af1ad754d61c9e21f05d853b765a3647
Sam <snippetsbysam@outlook.com>
2022-07-08 20:20:20 +0100

    Switch to AWS DB

--- EatSmart/EatSmart/Program.cs
+++ EatSmart/EatSmart/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using EatSmart.Models;
 using EatSmart.Services;
-
-
-
+using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +11,17 @@ builder.Services.AddScoped<IInputValidation, InputValidation>();
 builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("EatSmartApi");
+// AWS credentials
+var awsDbPassword = builder.Configuration["AWS-MYSQL-Db-password"];
+var awsCredentialsBuilder = new MySqlConnectionStringBuilder(
+    builder.Configuration.GetConnectionString("EatSmartAWSContext"));
+awsCredentialsBuilder.Password = builder.Configuration["AWS-MYSQL-Db-password"];
+var awsDbConnection = awsCredentialsBuilder.ConnectionString;
 
+builder.Services.AddDbContext<UserContext>(option => option.UseMySQL(awsDbConnection));
 
-builder.Services.AddDbContext<UserContext>(option =>
-    option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+//builder.Services.AddDbContext<UserContext>(option =>
+//option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Configure Swagger/OpenAPI Documentation
 builder.Services.AddEndpointsApiExplorer();
