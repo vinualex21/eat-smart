397d4fbccd52a9712b768be73bdb8034b5d7fda0
Sam <snippetsbysam@outlook.com>
2022-07-08 18:38:19 +0100

    update context to ensure DB creation

--- EatSmart/EatSmart/Models/UserContext.cs
+++ EatSmart/EatSmart/Models/UserContext.cs
@@ -7,6 +7,7 @@ namespace EatSmart.Models
     {
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
+            Database.EnsureCreated();
         }
        
         public DbSet<User> Users { get; set; }
