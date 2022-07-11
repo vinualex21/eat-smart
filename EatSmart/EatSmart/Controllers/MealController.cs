64baf296179bbc9dd996e66a0be2ab6b245a9f42
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-07-11 09:22:40 +0100

    Added condition for user not present

--- EatSmart/EatSmart/Controllers/MealController.cs
+++ EatSmart/EatSmart/Controllers/MealController.cs
@@ -24,15 +24,22 @@ namespace EatSmart.Controllers
         public ActionResult<List<MealDto>> GetMeals(long id)
         {
             User _user = _userService.GetUserById(id);
+
+            if (_user != null)
+            {
+                MealRequest _mealRequest = new MealRequest();
+                _mealRequest.MaxCalories = _user.MaxDailyCalories;
+                _mealRequest.Intolerances = _user.Intolerances;
+
+                var meals = new List<MealDto>();
+                meals = _mealService.Get3Meals(_mealRequest);
+
+                return meals;
+            }  
+            else
+                return NotFound($"User with Id {id} is not in the database");
+
             
-            MealRequest _mealRequest = new MealRequest();
-            _mealRequest.MaxCalories = _user.MaxDailyCalories;
-            _mealRequest.Intolerances = _user.Intolerances;
-
-            var meals = new List<MealDto>();
-            meals = _mealService.Get3Meals(_mealRequest);
-
-            return meals;
 
         }
     }
